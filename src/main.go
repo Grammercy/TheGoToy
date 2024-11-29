1ea4b5f5f45606fc7f7b32a4ccdc94fb2effe396
Grammercy <animated@paranoici.org>
2024-11-28 22:52:05 -0600

    now can render single particle types on grid :D

--- src/main.go
+++ src/main.go
@@ -10,7 +10,7 @@ type Board struct {
 }
 
 type Particle struct {
-	isEmpty   bool
+	isFull   bool
 	name      string
 	xVelocity float64
 	yVelocity float64
@@ -39,11 +39,45 @@ func main() {
 	}
 	defer window.Destroy()
 	defer renderer.Destroy()
+  running := true
+  renderer.SetDrawColor(0, 0, 0, 255)
+	renderer.Clear()
+  board.arr[3][3].isFull = true
+  for running {
+    board.render(renderer)
+    for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
+			switch event.(type) {
+			case *sdl.QuitEvent:
+				running = false
+			}
+		}
+    renderer.Present()
+    sdl.Delay(16)
+  }
+  fmt.Println("done")
+}
+
+func (board Board) render (renderer *sdl.Renderer) {
+  for i := range board.arr {
+    for j := range board.arr[i] {
+      x := int32(j * gridSize)
+      y := int32(i * gridSize)
+      renderer.SetDrawColor(176, 176, 176, 255)
+      renderer.FillRect(&sdl.Rect{X: x, Y: y, W: int32(gridSize), H: int32(gridSize)})
+
+      if board.arr[i][j].isFull {
+        renderer.SetDrawColor(255, 255, 255, 255)
+      } else {
+        renderer.SetDrawColor(0, 0, 0, 255)
+      }
+      renderer.FillRect(&sdl.Rect{X: x + borderWidth, Y: y + borderWidth, W: int32(gridSize) - borderWidth, H: int32(gridSize) - borderWidth})
+    }
+  }
 }
 
 func determineSquareSize(totalX, totalY, gridX, gridY int) int {
-  gridX += borderWidth
-  gridY += borderWidth
+  gridX += 2 * borderWidth
+  gridY += 2 * borderWidth
   gridSize := totalX/gridX
   if totalY/gridY < gridSize {
     gridSize = totalY/gridY
@@ -51,7 +85,7 @@ func determineSquareSize(totalX, totalY, gridX, gridY int) int {
   return gridSize
 }
 
-func (board Board) setupBoard() {
+func (board *Board) setupBoard() {
 	board.arr = make([][]Particle, 20)
 	for i := range board.arr {
 		board.arr[i] = make([]Particle, 20)
@@ -62,7 +96,7 @@ func (board Board) String() string {
 	str := ""
 	for i := range board.arr {
 		for j := range board.arr {
-			if !board.arr[i][j].isEmpty {
+			if !board.arr[i][j].isFull {
 				str += " "
 			} else {
 				str += "󰝤 "
